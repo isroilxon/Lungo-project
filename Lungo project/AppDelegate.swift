b14842114b9add05c861af1b20cb7301f3fec7a8
isroilxon <119227166+isroilxon@users.noreply.github.com>
2023-01-19 01:41:54 +0500

    Initial Commit

---
+++ Lungo project/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Lungo project
+//
+//  Created by mac on 18/01/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = FirsyViewController()
+        window?.rootViewController = UINavigationController(rootViewController: vc)
+        window?.makeKeyAndVisible()
+        return true
+    }
+    
+}
